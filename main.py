3841c30d064130bdc55b748a496c353f44f94dc7
Starman <brent.ryan.phillips@proton.me>
2026-04-14 20:57:55 -0700

    Adding validation function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 # Set format of MSH segment
 msh_format = {
     'MSH-1_Field Separator': "|",
@@ -60,3 +61,18 @@ split_msh = sample_msh.split('|')
 
 message_dict = dict(zip(msh_format_2, split_msh))
 print(message_dict)
+
+
+def find_invalid_records(
+        patient_id, age, gender, diagnosis, medications, last_visit_id
+):
+    constraints = {
+        'patient_id': isinstance(patient_id, str) and re.fullmatch('p\d+', patient_id, re.IGNORECASE),
+        'age': isinstance(age, int) and age >= 18,
+        'gender': isinstance(gender, str) and gender.lower() in ('male', 'female'),
+        'diagnosis': isinstance(diagnosis, str) or diagnosis is None,
+        'medications': isinstance(medications, list) and all([isinstance(i, str) for i in medications]),
+        'last_visit_id': isinstance(last_visit_id, str)
+
+    }
+    return constraints
